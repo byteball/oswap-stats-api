d579c81a4ecd9fd84abfd5ad672b852ff3c6661b
tony <tonyofbyteball@users.noreply.github.com>
2026-03-21 02:50:16 +0800

    new base AA

--- conf.js
+++ conf.js
@@ -14,7 +14,7 @@ exports.pathToDist = process.env.pathToDist || './dist/';
 exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.explorer_base_url = process.env.testnet ? 'https://testnetexplorer.obyte.org/#' : 'https://explorer.obyte.org/#';
 
-exports.oswap_base_aas = ["DYZOJKX4MJOQRAUPX7K6WCEV5STMKOHI", "2JYYNOSRFGLI3TBI4FVSE6GFBUAZTTI3"];
+exports.oswap_base_aas = ["V3U7V4HPGG2RILPLD3UUJDJS66CYICJJ", "DYZOJKX4MJOQRAUPX7K6WCEV5STMKOHI", "2JYYNOSRFGLI3TBI4FVSE6GFBUAZTTI3"];
 exports.token_registry_aa_address = "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ";
 
 console.log('finished server conf');
